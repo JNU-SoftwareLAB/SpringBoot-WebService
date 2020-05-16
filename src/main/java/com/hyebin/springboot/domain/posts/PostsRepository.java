90325f3f3d15ca0dba0614c29179c7cce29e343c
hyebin <whgpqls259@naver.com>
2020-05-16 15:26:28 +0900

    ~5.2 finished
    to .gitignore check

--- src/main/java/com/hyebin/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hyebin/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,11 @@ package com.hyebin.springboot.domain.posts;
 
 import com.hyebin.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/hyebin/springboot/service/PostsService.java
+++ src/main/java/com/hyebin/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.hyebin.springboot.service;
 
 import com.hyebin.springboot.domain.posts.Posts;
 import com.hyebin.springboot.domain.posts.PostsRepository;
+import com.hyebin.springboot.web.dto.PostsListResponseDto;
 import com.hyebin.springboot.web.dto.PostsResponseDto;
 import com.hyebin.springboot.web.dto.PostsSaveRequestDto;
 import com.hyebin.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,10 +10,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+
+        return new PostsResponseDto(entity);
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
@@ -28,10 +46,12 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
-        return new PostsResponseDto(entity);
+        postsRepository.delete(posts);
     }
+
 }
